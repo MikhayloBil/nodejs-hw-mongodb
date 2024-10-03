a2cfb35924361b664f6b92ebd7f74e9ba3834b4a
MikhayloBil <152810994+MikhayloBil@users.noreply.github.com>
2024-10-03 13:38:47 +0300

    upd

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -29,13 +29,13 @@ authRouter.post('/refresh', ctrlWrapper(authControllers.refreshController));
 authRouter.post('/logout', ctrlWrapper(authControllers.logoutController));
 
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(authControllers.requestResetEmailController),
 );
 
 authRouter.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(authControllers.resetPasswordController),
 );
